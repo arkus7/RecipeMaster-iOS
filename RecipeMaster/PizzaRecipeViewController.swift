f51363cd34c4c0d3e7e8540c79ef19ee509cd216
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-10-19 15:16:45 +0200

    Add recipe view

---
+++ RecipeMaster/PizzaRecipeViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PizzaRecipeViewController.swift
+//  RecipeMaster
+//
+//  Created by Arkadiusz Żmudzin on 19.10.2016.
+//  Copyright © 2016 Arkadiusz Żmudzin. All rights reserved.
+//
+
+import UIKit
+
+class PizzaRecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+
+}
