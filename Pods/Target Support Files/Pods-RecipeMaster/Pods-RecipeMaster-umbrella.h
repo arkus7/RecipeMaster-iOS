43b3db812978dc9a4d39257930d478deb8fd6f63
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-10-20 08:27:12 +0200

    Install pods

---
+++ Pods/Target Support Files/Pods-RecipeMaster/Pods-RecipeMaster-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_RecipeMasterVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RecipeMasterVersionString[];
+
